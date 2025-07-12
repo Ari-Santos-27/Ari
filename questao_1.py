3a2d152147bd6c377bc291914fe38848c4f29efe
Ari-Santos-27 <arirdizonz@gmail.com>
2025-07-12 17:51:06 -0300

    Add files via upload

---
+++ questao_1.py
@@ -0,0 +1,13 @@
+# Faça duas Variaveis numericas e descubra qual e a maior????????
+
+Num1 = 2344
+Num2 = 234
+
+if Num1 > Num2:
+    print(f"Numero: {Num1} e maior que o {Num2}...")
+
+elif Num2 > Num1:
+    print(f"Numero: {Num2} e maior que o {Num1}...")
+
+else:
+    print("Numeros iguais!")
